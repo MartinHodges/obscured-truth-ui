d303295d01257d44cb0e5f89fd1269d71d7b5377
Martin Hodges <martin.hodges@requillion-solutions.com>
2023-03-16 19:25:15 +1100

    Update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
-import { ReactElement } from "react"
 import { Box } from "@mui/material"
+import SearchIcon from '@mui/icons-material/Search';
 
 export default function Header() {
 
     return (
         <Box display='flex' justifyContent='center' sx={{minHeight: '48px', backgroundColor: 'purple', color: 'white'}}>
-            <h1>Obscured Truth!</h1>
+            <Box display='flex' alignItems='center'>
+                <h1>Obscured Truth!</h1>
+                <SearchIcon />
+            </Box>
         </Box>
     )
 }
