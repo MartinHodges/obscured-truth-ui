470f094b63cc3909a592d420d9791f7ffc477c68
Martin Hodges <martin.hodges@requillion-solutions.com>
2023-03-16 18:56:24 +1100

    Update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
 
     return (
         <Box display='flex' justifyContent='center' sx={{minHeight: '48px', backgroundColor: 'purple', color: 'white'}}>
-            <h1>Obscured Truth !</h1>
+            <h1>Obscured Truth!</h1>
         </Box>
     )
 }
